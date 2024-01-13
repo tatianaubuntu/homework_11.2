2a4d4857661eab31a218fa7d8966ca693f94708b
Tatiana <tatiana@Ubuntu.myguest.virtualbox.org>
2024-01-13 19:15:28 +0500

    add new func and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def string(line):
+    '''Получение строки заглавными буквами'''
     return line.upper()
+
+
+def string1(line):
+    '''Получение строки. Первая буква слова заглавная'''
+    return line.title()
